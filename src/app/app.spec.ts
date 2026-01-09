2bb902efd05715a4fbc3b7416ab5b88354e2d0ea
Sergio Rodriguez <sergiorod977@gmail.com>
2026-01-08 23:50:52 -0600

    correciones

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, proyecto-angular-netlify');
+    expect(compiled.querySelector('h1')?.textContent).toContain('¡Mensaje desde angular!');
   });
 });
